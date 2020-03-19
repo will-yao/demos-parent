44b3cb074e59fafed210b5ec377f202c4088df23
xuan tuo <yunzheng.yyz@alibaba-inc.com>
2020-03-19 20:32:04 +0800

    + 枚举类反射

--- common/src/main/java/com/xuantuo/reflect/EnumTest.java
+++ common/src/main/java/com/xuantuo/reflect/EnumTest.java
@@ -13,16 +13,19 @@ public class EnumTest {
 
     public static void main(String[] args) {
         try {
+
             Constructor cstr = HumanState.class.getDeclaredConstructor(
-                    String.class, int.class
+                    String.class,int.class,long.class, String.class, String.class
             );
+            //Constructor cstr = HumanState.class.getDeclaredConstructors()[0];
             ReflectionFactory reflection =
                     ReflectionFactory.getReflectionFactory();
             HumanState e =
-                    (HumanState) reflection.newConstructorAccessor(cstr).newInstance(new Object[]{"ANGRY", 3});
+                    (HumanState) reflection.newConstructorAccessor(cstr).newInstance(new Object[]{"ANGRY",-1, 88L, "a", "a"});
             System.out.printf("%s = %d\n", e.toString(), e.ordinal());
 
         } catch (Exception e) {
+            System.out.println(e.getStackTrace());
 
         }
 
--- common/src/main/java/com/xuantuo/reflect/HumanState.java
+++ common/src/main/java/com/xuantuo/reflect/HumanState.java
@@ -5,5 +5,16 @@ package com.xuantuo.reflect;
  * @Date: Created on 2020/3/19
  **/
 public enum HumanState {
-    HAPPY, SAD;
+    HAPPY(1L,"h","h"), SAD(2L,"s","s");
+
+
+    private long l;
+    private String s1;
+    private String s2;
+
+    HumanState(long l, String s1, String s2) {
+        this.l = l;
+        this.s1 = s1;
+        this.s2 = s2;
+    }
 }
