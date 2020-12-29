970f222442ede7ea391e085197cba16fb0d04fdb
xuan tuo <yunzheng.yyz@alibaba-inc.com>
2020-12-29 22:42:31 +0800

    - 多余依赖

--- start/src/main/java/com/xuantuo/portal/web/Test1StarterController.java
+++ start/src/main/java/com/xuantuo/portal/web/Test1StarterController.java
@@ -1,6 +1,6 @@
 package com.xuantuo.portal.web;
 
-import com.xuantuo.portal.DemoService;
+//import com.xuantuo.portal.DemoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +13,12 @@ import javax.annotation.Resource;
 @RestController
 public class Test1StarterController {
 
-    @Resource
-    private DemoService demoService;
+//    @Resource
+//    private DemoService demoService;
 
 
-    @GetMapping("/say")
-    public String sayWhat() {
-        return demoService.say();
-    }
+//    @GetMapping("/say")
+//    public String sayWhat() {
+//        return demoService.say();
+//    }
 }
